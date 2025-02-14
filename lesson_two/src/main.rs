4499be43fa14f5aeebcacfa8266114e8f5a3db0b
Ivan Kleimenov <ivankleimenov@Ivans-MacBook-Pro.local>
2025-02-14 16:08:49 -0500

    loops and conditions part 1

---
+++ lesson_two/src/main.rs
@@ -0,0 +1,12 @@
+// type of loops
+mod utils;
+
+fn main() {
+    utils::loops::_loop();
+    println!("---------------------------------");
+    utils::loops::_while();
+    println!("---------------------------------");
+    utils::loops::_for_loop();
+    println!("---------------------------------");
+    utils::loops::_for_loop_II();
+}
