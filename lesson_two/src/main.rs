47cb6f386069e5a17811ba88f2b8c59bb7df1f7a
Ivan Kleimenov <ivankleimenov@Ivans-MacBook-Pro.local>
2025-02-18 09:35:06 -0500

    explore destructure reference and dereferences approach

--- lesson_two/src/main.rs
+++ lesson_two/src/main.rs
@@ -2,11 +2,19 @@
 mod utils;
 
 fn main() {
-    utils::loops::_loop();
-    println!("---------------------------------");
-    utils::loops::_while();
-    println!("---------------------------------");
-    utils::loops::_for_loop();
-    println!("---------------------------------");
-    utils::loops::_for_loop_II();
+    // utils::loops::_loop();
+    // println!("---------------------------------");
+    // utils::loops::_while();
+    // println!("---------------------------------");
+    // utils::loops::_for_loop();
+    // println!("---------------------------------");
+    // utils::loops::_for_loop_II();
+    // println!("---------------------------------");
+    let mut numbers = [1, 2, 3, 4, 5];
+    let result = utils::loops::fnnd_max_destructuring_the_reference(numbers);
+    println!("Result: {:?}", result);
+    // let mut numbers = [1, 2, 3, 4, 5];
+    // println!("Initial array: {:?}", numbers);
+    // let result = utils::loops::mutation_element(&mut numbers);
+    // println!("Result: {:?}", result);
 }
